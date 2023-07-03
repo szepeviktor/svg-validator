01fc8e2de47fca98ce9b4ce001dfaa443422d88d
Viktor Szépe <viktor@szepe.net>
2023-07-03 07:35:12 +0000

    Require UTF-8 encoding

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is an action for GitHub Actions. It runs `xmllint` on every matching SVG fi
 
 ```yaml
     - name: "Validate SVG files"
-      uses: "szepeviktor/svg-validator@v0.1.0"
+      uses: "szepeviktor/svg-validator@v1.0.0"
       with:
         svg_path: "public/**/*.svg"
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ set -e -o pipefail
 
 shopt -s globstar dotglob nullglob
 
-SVG_XML_DECLARATION='<?xml version="1.0" standalone="no"?>'
+SVG_XML_DECLARATION='<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
 SVG_DOCTYPE='<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
 SVG_NS_URL='http://www.w3.org/2000/svg'
 SVG_VERSION='1.1'
